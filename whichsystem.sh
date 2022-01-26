22ec03e3b60011a499d951aa9a39a9f65ac144a3
KevinGuzmanH <kevinalternativa1@gmail.com>
2022-01-26 11:05:34 -0500

    TTL comparison

--- whichsystem.sh
+++ whichsystem.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-#ip
+ttl=$(ping -c1 $1 | grep 'ttl' | awk '{print $6}' | cut -c5-7 )
+
+if [ $ttl -ge 0 -a $ttl -lt 65 ] ;
+then
+   echo 'Linux'
+elif [ $ttl -ge 64 -a $ttl -lt 129 ];then
+   echo 'Windows'
+fi
+
+
+
+
